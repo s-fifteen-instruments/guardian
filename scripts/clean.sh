ddc6877700b99b518183136e9994b4ee7f272644
Cyrus Proctor <sidio47@hotmail.com>
2021-06-03 13:14:29 -0500

    Clean up after new cert gen.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -33,6 +33,7 @@ if [ "${1}" = "kme1" ] || [ "${1}" = "both" ]; then
   rm -rf ./volumes/kme1/certificates/production/vault_init
   rm -rf ./volumes/kme1/certificates/production/rest
   rm -rf ./volumes/kme1/certificates/production/watcher
+  rm -rf ./volumes/kme1/certificates/production/sae
   rm -rf ./volumes/kme1/certificates/production/admin
   rm -rf ./volumes/kme1/certificates/generation/root
   rm -rf ./volumes/kme1/vault/data/file
@@ -41,6 +42,8 @@ if [ "${1}" = "kme1" ] || [ "${1}" = "both" ]; then
   rm -f  ./volumes/kme1/vault/policies/rest.policy.hcl
   rm -rf ./volumes/kme1/qkd/epoch_files/*
   rm -rf ./volumes/kme1/qkd/digest_files/*
+  # For remote client CA chain
+  rm -rf ./volumes/kme2/certificates/production/rest
 
 fi
 if [ "${1}" = "kme2" ] || [ "${1}" = "both" ]; then
@@ -50,6 +53,7 @@ if [ "${1}" = "kme2" ] || [ "${1}" = "both" ]; then
   rm -rf ./volumes/kme2/certificates/production/vault_init
   rm -rf ./volumes/kme2/certificates/production/rest
   rm -rf ./volumes/kme2/certificates/production/watcher
+  rm -rf ./volumes/kme2/certificates/production/sae
   rm -rf ./volumes/kme2/certificates/production/admin
   rm -rf ./volumes/kme2/certificates/generation/root
   rm -rf ./volumes/kme2/vault/data/file
@@ -58,5 +62,7 @@ if [ "${1}" = "kme2" ] || [ "${1}" = "both" ]; then
   rm -f  ./volumes/kme2/vault/policies/rest.policy.hcl
   rm -rf ./volumes/kme2/qkd/epoch_files/*
   rm -rf ./volumes/kme2/qkd/digest_files/*
+  # For remote client CA chain
+  rm -rf ./volumes/kme1/certificates/production/rest
 
 fi
