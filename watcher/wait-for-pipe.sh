2efd7e2ea571ca5c0dd7a3a9dd3cd0932d690f22
pyuxiang <pehyuxiang@gmail.com>
2022-07-01 15:40:49 +0800

    Add pipe existence check before starting watcher
    watcher internally already has a loop to check for pipe existence, but there are occasional observations of watcher not responding to piped data. Adding a guarantee to see if this helps resolve the issue.

---
+++ watcher/wait-for-pipe.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Justin, 2022-07-01
+#
+# watcher seems to require notify.pipe to be up before starting,
+# but the Compose option of conditional 'depends_on' works only for
+# Compose >= 3.9 or < 3.0.
+#
+# Alternative suggested in 'https://docs.docker.com/compose/startup-order/'
+# to implement a 'wait-for-it.sh' equivalent script to prepend in CMD.
+set -e
+
+# Retrieve required parameters
+PIPE="$1"
+shift
+
+# Repeatedly check existence of file
+# For host environment, use docker exec -it "$SERVICE" sh -c "test -p $PIPE"
+until [ -p $PIPE ]; do
+        >&2 echo "Notification pipe not up - sleeping..."
+        sleep 1
+done
+>&2 echo "Notification pipe up!"
+exec "$@"
+
