e95162733c7e46c65c9a0f11a8baefced097c991
Syed Abdullah Aljunid <syed.aljunid@gmail.com>
2023-01-12 21:19:11 +0800

    Added exception for certs without sae-id. This comes from remote kme doing a ledger put

--- rest/app/utils/client.py
+++ rest/app/utils/client.py
@@ -49,6 +49,9 @@ def parse_sae_client_info(request: Request) -> typing.Dict:
         # Remove backslashes and double quotes
         sae_id = sae_id.strip('\\"')
         # logger.debug(f"Got {sae_id} as sae ID")
+    except IndexError:
+        #No sae-id: in cert
+        sae_id = ""
     except Exception as err:
         logger.warn(f"Unparsable sae ID in sae certificate:\n{sae_id}")
         logger.exception(err)
