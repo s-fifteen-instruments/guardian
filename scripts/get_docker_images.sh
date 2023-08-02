0e6f123ac27b979f04af1d5603fc6475f1c2925b
Syed Abdullah Aljunid <syed.aljunid@gmail.com>
2023-08-03 00:27:21 +0800

    Initial commit without downloaded files

---
+++ scripts/get_docker_images.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+
+images_dir=/tmp/docker_images
+mkdir -p $images_dir
+image_list="python:3.9-slim alpine python:3.9-alpine traefik hashicorp/vault"
+for each in $image_list
+do
+  docker image pull $each;
+  each2=`echo $each | sed s/"\/"/"-"/g `
+  docker save $each > $images_dir/$each2
+done
+
+
+tag=ap_copy_things
+# Certauth
+
+list="make openssl"
+docker run -d --rm --name $tag alpine /bin/sh -c "mkdir -p /apk && cd /apk && apk update && apk fetch -R $list && tail -f /dev/null"
+docker exec $tag /bin/sh -c "cd /apk && apk fetch -R python3 py-pip docker openrc && tail -f /dev/null"
+sleep 10
+
+# Unsealer
+docker exec $tag /bin/sh -c "cd /apk && apk fetch -R python3 py-pip docker openrc && tail -f /dev/null"
+sleep 30
+
+docker cp $tag:/apk $images_dir
+
+
+
+
+docker stop $tag
+
+tag=py_copy_things
+list="build-essential inetutils-ping"
+docker run -d --rm --name $tag python:3.9-slim /bin/bash -c "mkdir -p /deb && cd /deb && apt-get update && \
+       	apt-get install --dry-run --no-install-recommends ${list} > /tmp/apt_list && \
+       	sed -n '/The following NEW packages will be installed:/,/upgraded/p' /tmp/apt_list | tail -n+2 | head -n-1 | tr '\n' ' ' > /tmp/apt_list2 && \
+	tail -f /dev/null"
+
+docker exec $tag /bin/sh -c "cd /deb && cat /tmp/apt_list2  | xargs apt-get download"
+sleep 60
+docker cp $tag:/deb $images_dir
